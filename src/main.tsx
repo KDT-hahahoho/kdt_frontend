0a85e189e5fa30876d25609d64478bceb49df3c4
woojoung <ywj981217@naver.com>
2024-10-11 21:42:24 +0900

    Chore : react-query 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
+/* eslint-disable */
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </QueryClientProvider>
 );
