b0170fae4983edf85b65e06b95eec766eba3d62e
woojoung <ywj981217@naver.com>
2024-10-11 13:34:18 +0900

    Docs : 보일러플레이트 생성

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+  background-color: #3498db;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #2980b9;
+  }
+`;
+
+function App() {
+  return <Button>Click me</Button>;
+}
+
+export default App;
