46c2bdf7b9db66d7fdd858be544e8edbcb3880e2
s0zzang <dlth8888@gmail.com>
2024-10-17 18:05:52 +0900

    Style: global, variable, layout 세팅

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,45 +1,264 @@
-
-
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import variables from './Variables';
 
 const GlobalStyles = css`
-  /* 기본 스타일 */
+  :root {
+    font-size: 10px; /* 1rem = 10px */
+    width: 100vw;
+    min-width: 320px;
+    overflow: hidden;
+    background: ${variables.colors.gray50};
+  }
+
   body {
-    margin: 0;
-    padding: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-    background-color: #f0f0f0; /* 배경색 */
-    color: #333; /* 글자색 */
-    overflow-x: hidden; /* 수평 스크롤 방지 */
+    margin: unset;
+    color: ${variables.colors.black};
+    font-family: var(--font-roboto), var(--font-noto), sans-serif;
+    font-size: ${variables.size.medium};
+  }
+
+  html,
+  div,
+  span,
+  applet,
+  object,
+  iframe,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  blockquote,
+  pre,
+  a,
+  abbr,
+  acronym,
+  address,
+  big,
+  cite,
+  code,
+  del,
+  dfn,
+  em,
+  img,
+  ins,
+  kbd,
+  q,
+  s,
+  samp,
+  small,
+  strike,
+  strong,
+  sub,
+  sup,
+  tt,
+  var,
+  b,
+  u,
+  i,
+  center,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  legend,
+  table,
+  caption,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  article,
+  aside,
+  canvas,
+  details,
+  embed,
+  figure,
+  figcaption,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  output,
+  ruby,
+  section,
+  summary,
+  time,
+  mark,
+  audio,
+  video {
+    margin: unset;
+    padding: unset;
+    border: 0;
+    font-family: sans-serif;
+    font-size: inherit;
+    font-weight: inherit;
+    vertical-align: baseline;
+    box-sizing: border-box;
+    letter-spacing: -0.02em;
+    line-height: 1.4;
+    word-break: keep-all;
+  }
+  article,
+  aside,
+  details,
+  figcaption,
+  figure,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  section {
+    display: block;
+  }
+  ol,
+  ul {
+    list-style: none;
+  }
+  blockquote,
+  q {
+    quotes: none;
+  }
+  blockquote:before,
+  blockquote:after,
+  q:before,
+  q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
   }
 
   a {
+    color: inherit;
     text-decoration: none;
-    color: inherit; /* 링크 색상 상속 */
+    font-size: inherit;
   }
 
-  /* 모바일 환경 스타일 */
-  @media (max-width: 768px) {
-    body {
-      font-size: 14px; /* 모바일 기본 폰트 크기 */
-    }
-
-    h1 {
-      font-size: 24px; /* 모바일 H1 크기 */
-    }
-
-    h2 {
-      font-size: 20px; /* 모바일 H2 크기 */
-    }
-
-    /* 추가 모바일 스타일 */
-    .container {
-      padding: 16px; /* 모바일 패딩 */
-    }
+  strong {
+    font-weight: bold;
   }
 
-  /* 추가 스타일을 여기에 작성 */
+  button {
+    all: unset;
+    cursor: pointer;
+  }
+
+  label {
+    cursor: pointer;
+  }
+
+  /* input, select style */
+  select {
+    color: inherit;
+    appearance: none;
+    background: url(/img/icon-accordion-blue.svg) no-repeat center right var(--title-medium) / 1rem;
+  }
+
+  select,
+  input {
+    font-family: var(--font-roboto), var(--font-noto), 'sans-serif';
+    letter-spacing: -0.02em;
+    box-sizing: border-box;
+    padding: 0;
+  }
+
+  select,
+  input[type='text'],
+  input[type='email'],
+  input[type='password'],
+  input[type='search'],
+  input[type='tel'] {
+    height: 4.4rem;
+    width: 100%;
+    font-size: ${variables.size.medium};
+    text-indent: ${variables.size.medium};
+    border-radius: ${variables.borderRadius};
+    border: 1px solid ${variables.colors.gray50};
+  }
+
+  input[type='text']::placeholder,
+  input[type='email']::placeholder,
+  input[type='password']::placeholder,
+  input[type='tel']::placeholder {
+    color: ${variables.colors.gray50};
+    font-family: unset;
+  }
+
+  input[type='text']:focus,
+  input[type='email']:focus,
+  input[type='password']:focus,
+  input[type='tel']:focus {
+    border-color: ${variables.colors.primary};
+    outline: 1px solid ${variables.colors.primary};
+  }
+
+  select:focus-visible {
+    border-color: ${variables.colors.primary};
+    outline: 1px solid ${variables.colors.primary};
+  }
+
+  /* checkbox style */
+  input[type='checkbox'],
+  input[type='radio'] {
+    all: unset;
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    vertical-align: text-top;
+    border-radius: 0.2em;
+    width: 1em;
+    aspect-ratio: 1 / 1;
+    background: ${variables.colors.primarySoft};
+    margin-right: 0.5em;
+    box-sizing: border-box;
+  }
+
+  input[type='checkbox']:checked {
+    background: ${variables.colors.primary} url(/img/icon-check.svg) no-repeat center / 70%;
+  }
+
+  /* radio style */
+  input[type='radio']:checked {
+    border: 0.15em solid ${variables.colors.primary};
+  }
+
+  input[type='radio']:after {
+    content: '';
+    display: block;
+    position: absolute;
+    width: calc(100% - 0.22em);
+    aspect-ratio: 1/1;
+    background: ${variables.colors.primary};
+    opacity: 0;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  input[type='radio']:checked:after {
+    opacity: 1;
+  }
+
+  @media (max-width: 350px) {
+    :root {
+      font-size: 9px;
+    }
+  }
 `;
 
 export default GlobalStyles;
---
+++ src/styles/Variables.tsx
@@ -0,0 +1,37 @@
+const variables = {
+  colors: {
+    primary: '#ccb2ff',
+    primaryStrong: '#b691ff',
+    primarySoft: '#eee5ff',
+    secondary: '#ffd4cb',
+    secondaryStrong: '#ffa997',
+    secondarySoft: '#ffefeb',
+    tertiary: '#ffa954',
+    tertiarySoft: '#ffedda',
+    black: '#000',
+    gray100: '#737980',
+    gray50: '#d9d9d9',
+    gray10: '#f5f5f5',
+    white: '#fff',
+  },
+
+  size: {
+    title: '2.4rem',
+    max: '2.2rem',
+    extra: '2rem',
+    large: '1.8rem',
+    big: '1.6rem',
+    medium: '1.4rem',
+    small: '1.2rem',
+    min: '1rem',
+  },
+
+  layoutPadding: '1.8rem',
+  maxLayout: '500px',
+  headerHeight: '5.6rem',
+  TransitionDuration: '0.3s',
+  BoxShadow: '0 0 0.5rem rgba(0, 0, 0, 0.15)',
+  borderRadius: '.8rem',
+};
+
+export default variables;
